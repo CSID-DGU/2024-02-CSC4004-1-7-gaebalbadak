0f5cd7b4527cc70f964a9b43fd6870d79b6267b6
jorepong123 <jorepong123@naver.com>
2024-11-05 20:32:49 +0900

    docs: 테스트

--- backend/manage.py
+++ backend/manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
@@ -13,7 +12,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment? test"
         ) from exc
     execute_from_command_line(sys.argv)
 
