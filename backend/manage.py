d108f7000e52fa3f8d10f5811ea4f0d16616f87d
jorepong123 <jorepong123@naver.com>
2024-11-07 01:04:24 +0900

    feat: 테스트3

--- backend/manage.py
+++ backend/manage.py
@@ -12,7 +12,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment? test"
+            "forget to activate a virtual environment? test test3"
         ) from exc
     execute_from_command_line(sys.argv)
 
