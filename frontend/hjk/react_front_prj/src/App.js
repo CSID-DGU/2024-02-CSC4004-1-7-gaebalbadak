32e4a1df3b23de5b0a40440dc6f9cfd6f3d38e05
xiest <xiest@dgu.ac.kr>
2024-11-21 13:37:19 +0900

    feat.%toPx

--- frontend/hjk/react_front_prj/src/App.js
+++ frontend/hjk/react_front_prj/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Filter from './pages/Filter';
 import Details from './pages/Details';
-import NavBarElements from './components/NavBarElements';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
-      <NavBarElements></NavBarElements>
-
-      <Routes>
+       <Routes>
         <Route path = "/" element = { <Main /> } />
         <Route path = "/Main" element = { <Main /> } />
         <Route path = "/Filter" element = { <Filter /> } />
         <Route path = "/Details" element = { <Details /> } />
       </Routes>
+
     </Router>
 
   );
--- frontend/hjk/react_front_prj/src/pages/Details.js
+++ frontend/hjk/react_front_prj/src/pages/Details.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import styles from './Details.module.css'
+import logo from '../assets/img/detail-icon-img.png'
+import ping from '../assets/img/ping.png'
 
 const Details = () => {
   return (
-    <div>
-        details
+    <div className={styles.wrapper}>
+        <div className={styles.header}></div>
+        <div className={styles.contents}>
+          <img src={logo} className={styles.logo_img} alt='logo'></img>
+
+          <div className={styles.restaurants_name_area}>
+              <img src={ping} className={styles.ping_img} alt='ping'></img>
+              <div className={styles.nameText}>서울카츠</div>
+          </div>
+          <div className={styles.contents_area}>
+            <div className={styles.review_area}>
+              
+            </div>
+            <div className={styles.overview_area}>
+              
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
