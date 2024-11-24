77888f08a8192d240855ad4a1fd24a24194d3934
jorepong123 <jorepong123@naver.com>
2024-11-24 19:31:30 +0900

    feat: 리뷰 라벨링 도구 추가

--- backend/reviews/internal_tasks/incentivized_review_classifier.py
+++ backend/reviews/internal_tasks/incentivized_review_classifier.py
@@ -35,12 +35,13 @@ def analyze_reviews(reviews):
                                               "AI입니다."
                                               "'가성비갑', '리뷰이벤트', '포토리뷰'와 같은 항목은 해당 이름의 메뉴를 주문함으로써"
                                               "대가를 제공받았음을 의미합니다."
+                                              "하지만 메뉴명이 '가성비'인 경우가 아닌, '가성비'라는 단어가 포함된 음식 메뉴라면 대가성 리뷰가 아닙니다."
                                               "리뷰가 대가성 리뷰라고 판단되면 is_incentivized_review 값으로 True를, 아니라면 False를 반환해주세요."
                                               "또한 대가성 리뷰라고 판단했다면 그 판단의 근거가 된 메뉴를 based_text로 반환해주세요."}]
 
     review_menus_str = ''
     for index, review in enumerate(reviews):
-        review_menus_str += f'{index+1}. {review.selected_menu}\n\n'
+        review_menus_str += f'{index}. {review.selected_menu}\n\n'
     review_menus_str = review_menus_str.strip()
 
     messages.append({"role": "user", "content": review_menus_str})
@@ -54,8 +55,6 @@ def analyze_reviews(reviews):
     content = message.content
     data = json.loads(content)
     return data
-    print(data)
-    # return response.choices[0].message
 
 def analyze_and_store_reviews(reviews):
     data = json.loads(analyze_reviews(reviews).content)
@@ -63,18 +62,32 @@ def analyze_and_store_reviews(reviews):
 
 
 def analyze_and_store_all_reviews():
-    reviews = Review.objects.all()[:30]
+    reviews = Review.objects.filter(
+        Q(selected_menu__isnull=False) & Q(manual_true_label_attempted=0)
+    ).order_by('?')
 
-    size = 30
-    index = 30
-    while True:
-        result = analyze_reviews(reviews)
+    batch_size = 25
+    total_reviews = len(reviews)  # QuerySet 전체 개수 계산
+    offset = 0  # 시작점
 
-        for i in range(size):
-            print(i)
-            print(reviews[i].selected_menu)
+    while offset < total_reviews:
+        # 30개씩 꺼내오되, 남은 데이터가 30개 미만일 경우 나머지를 가져옴
+        selected_reviews = reviews[offset:offset + batch_size]
+        result = analyze_reviews(selected_reviews)
+
+        # 분석 결과 출력
+        for i in range(len(selected_reviews)):
+            print(f'{i + 1}. {selected_reviews[i].restaurant.name}')
             print(result['sentiments'][i]['is_incentivized_review'])
-            print(result['sentiments'][i]['based_text'])
+            print(selected_reviews[i].selected_menu, '>>', result['sentiments'][i]['based_text'])
+            print()
+
+        # offset을 batch_size만큼 증가
+        offset += batch_size
+
+        # 모든 데이터를 처리했다면 종료
+        if len(selected_reviews) < batch_size:
+            break
 
 if __name__ == '__main__':
     # analyze_reviews()
--- backend/reviews/test.py
+++ backend/reviews/test.py
@@ -1,18 +1,63 @@
 import os
+from urllib.parse import urlencode
+
 import django
+import requests
+
+import ssh_manager
+from utils.string_utils import calculate_similarity
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
-import ssh_manager
-
 from reviews.models import Review, Restaurant, RestaurantPlatformInfo, Platform
 
-# 리뷰 가져오기 (최대 100개)
-reviews = Review.objects.all()
+def get_shop_identifier_id(target_shop_name, latitude, longitude):
+    target_shop_name = target_shop_name.replace('역점', '')
 
-# 조건을 만족하는 리뷰 필터링
-filtered_reviews = [review for review in reviews if len(review.content) >= 20]
+    base_url = 'https://api.coupangeats.com/endpoint/store.get_search'
+    query_params = {
+        'keyWord': target_shop_name,
+        'latitude': latitude,
+        'longitude': longitude,
+        'sort': 'nearby'
+    }
 
-# 조건에 해당하는 리뷰 수 출력
-print(f"리뷰 총 {len(filtered_reviews)}개가 조건에 해당합니다.")
+    headers = {
+        "X-EATS-LOCALE": "ko",
+        "X-EATS-LOCATION": f'{{"latitude": {latitude}, "longitude": {longitude}}}',
+        "X-EATS-OS-TYPE": "iOS",
+        "Accept": "*/*",
+        "Accept-Language": "ko-KR;q=1.0, en-KR;q=0.9",
+        "Host": "api.coupangeats.com",
+        "X-EATS-TIME-ZONE": "Asia/Seoul",
+        "X-EATS-DEVICE-ID": "4F30DECC-2E9B-4F24-884B-2DE8E85C086D/com.coupang.coupang-eats",
+        "X-EATS-PCID": "4F30DECC-2E9B-4F24-884B-2DE8E85C086D",
+        "X-EATS-APP-VERSION": "1.4.86",
+        "X-EATS-NETWORK-TYPE": "WiFi",
+        "Connection": "keep-alive",
+        "User-Agent": "Eats-AppStore/1.4.86 (com.coupang.coupang-eats; build:11956; iOS 18.0.1) Alamofire/5.9.1",
+        "Authorization": "Bearer eyJraWQiOiJjMjM3NDM1OC1lYzZlLTRkNjgtOTFlNS0zMjVkM2I4YjVkMmMiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHQiOnsiUENJRCI6ImVmNmM5YmI5MTY4NDczNGQxMDc0ZTYxM2FlMGRhZDcxIiwiYXV0aEJ5Ijoic2Vzc2lvbktleSIsImZpYXQiOjEuNzExNjE2MTQ0RTksInYiOiIxIiwiTFNJRCI6IjA5ZWRkYWRhLWUyOTUtNGE0Ny05M2ZkLTFlZTdmNGYwYTE1ZCIsIkZBR0UiOiI1OSJ9LCJzdWIiOiIxNDAxMTYyNzciLCJhdWQiOlsiaHR0cHM6Ly93d3cuY291cGFuZ2VhdHMuY29tIiwiaHR0cHM6Ly93d3cuY291cGFuZy5jb20iXSwic2NwIjpbIm9mZmxpbmUiLCJvcGVuaWQiLCJlYXRzIiwiY29yZS1zaGFyZWQiLCJwYXkiXSwibmJmIjoxNzMxOTM4MDI3LCJpc3MiOiJodHRwczovL21hdXRoLmNvdXBhbmcuY29tLyIsImV4cCI6MTczMTk1MjQyNywiaWF0IjoxNzMxOTM4MDI3LCJqdGkiOiIyYmVhYTc3NS0xNmQ1LTQzY2ItOGQwZS05ZTZlMDU5OWUwMTgiLCJjbGllbnRfaWQiOiJlNGNkOGZhYi0zM2ViLTRmNjQtODJjNC05MDQwMGNkNmNmZjgifQ.F4JAJYtvAooAwuN6KtPjgg_lZO5G6C4b3S6CI5yuLGrQINltmrN0KCrTnXmd0Tmu3-ASJYNE_q5WBP1-UhvOVQ",
+    }
+
+    url = f"{base_url}?{urlencode(query_params)}"
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    if ('text' in data['data']['entityList'][0]['entity']['data'] and
+        data['data']['entityList'][0]['entity']['data']['text'][0]['text'] == "검색 결과가 없습니다."):
+        return None
+
+    try:
+        shop_name = data['data']['entityList'][1]['entity']['data']['name']
+    except KeyError:
+        return get_shop_identifier_id(' '.join(target_shop_name.split(' ')[:-1]), latitude, longitude)
+
+    if calculate_similarity(target_shop_name, shop_name) > 0.7:
+        return data['data']['entityList'][1]['entity']['data']['id']
+    else:
+        return None
+
+
+
+print(get_shop_identifier_id('필동커피', 37.560078, 126.992633))
