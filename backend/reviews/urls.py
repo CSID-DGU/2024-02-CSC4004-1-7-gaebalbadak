658951fed12806253ac5b1bd06500f1c1663a7c0
suyeoni <tndusslarj@gmail.com>
2024-11-25 20:18:27 +0900

    feat: 필터기능API 추가

--- backend/reviews/urls.py
+++ backend/reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from reviews.views import AutoCompleteAPIView
+from reviews.views import AutoCompleteAPIView, FilterRestaurantsByCategoryAPIView
 
 urlpatterns = [
     path('api/autoComplete/', AutoCompleteAPIView.as_view(), name='autocomplete'),
+    path('api/restaurants/filter/', FilterRestaurantsByCategoryAPIView.as_view(), name='filter_restaurants'),
 ]
